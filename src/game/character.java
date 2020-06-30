a0369f974cd422da9832c93d41310d65002ddadb
YuJeong03 <63700054+YuJeong03@users.noreply.github.com>
2020-06-30 11:55:57 +0900

    commit1

---
+++ src/game/character.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.ArrayList;
+
+public class character {
+	
+	private String name;
+	private int level;
+	
+	private ArrayList<item> items;
+	private ArrayList<skill> skils;
+	
+	public character (String name) {
+		this.name = name;
+		this.level = level;
+	}
+	
+}
--- src/game/item.java
+++ src/game/item.java
@@ -1,5 +1,15 @@
 package game;
 
 public class item {
-
+	private String mname;
+	private int mitemcode;
+	private int mitemCount;
+	private long mGUID;
+	
+	public item(String name, int itemcode, int guid, int itemcount) {
+		mname = name;
+		mitemcode = itemcode;
+		mGUID = guid;
+		mitemCount = itemcount;		
+	}
 }
